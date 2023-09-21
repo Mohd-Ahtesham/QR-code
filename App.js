594431cee3993710179b6f2dfa19e431de7355a2
mac <mac@Danish-MacBook-Pro.local>
2023-09-21 16:33:28 +0530

    add splash screen in android

--- App.js
+++ App.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Navigation from './src/screens/Navigation';
+import SplashScreen from 'react-native-splash-screen';
+import {Platform} from 'react-native';
 
 const App = () => {
+  useEffect(() => {
+    if (Platform.OS === 'android') {
+      SplashScreen.hide();
+    }
+  }, []);
   return <Navigation />;
 };
 export default App;
